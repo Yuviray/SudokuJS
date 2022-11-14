6535b3e61d60f2e3461fb5c4f4586fb46397bd07
Yuvanesh Rajamani <jjd149@txstate.edu>
2022-11-14 14:53:17 -0600

    adding Jest

--- phpTest/ConfigTest.php
+++ phpTest/ConfigTest.php
@@ -7,7 +7,7 @@ class ConfigTest extends TestCase {
         $config = new Tester\ConfigClass("empty","hello","empty","empty");
         $result = $config->getRoot("hello");
 
-        // Yuvanesh's firsttime unit testng with php
+        // Yuvanesh's first time unit testng with php
         // we only have 1 php class to test and its simple
         // to run tests use this command: ./vendor/bin/phpunit
         $this->assertNotEmpty($result);
