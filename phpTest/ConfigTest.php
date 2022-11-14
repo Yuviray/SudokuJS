5abb6f7751a0a7a21e73eb768c76a837b0f0e50e
Waleed Naveed <102765136+wnaveed5@users.noreply.github.com>
2022-11-14 17:02:09 -0600

    Waleeds PHP test

--- phpTest/ConfigTest.php
+++ phpTest/ConfigTest.php
@@ -12,6 +12,17 @@ class ConfigTest extends TestCase {
         // to run tests use this command: ./vendor/bin/phpunit
         $this->assertNotEmpty($result);
         $this->assertEquals("hello",$result);
+
+
+        $config_test = new Tester\ConfigClass("host","root","pass","db");
+ 
+        //Waleed's Test
+        $this->assertSame("host", $config_test->getHost());
+        $this->assertSame("root", $config_test->getRoot());
+        $this->assertSame("pass", $config_test->getPass());
+        $this->assertSame("db", $config_test->getDb());
+       
+       
        
     }
  }
